ff0e34df8b40c27bc72250c219465e2d13a756fd
oa <Narroe@users.noreply.github.com>
2017-01-03 09:21:58 +0000

    finished initial draft
    works as intended as long as the information provided is in the correct format and range.
    (e.g. month number doesn't exceed 12.)

--- Determination of the day of the week/DeterminationOfTheDayOfTheWeek.cpp
+++ Determination of the day of the week/DeterminationOfTheDayOfTheWeek.cpp
@@ -1,30 +1,54 @@
-/*NOT CURRENTLY WORKING*/
-// Day Name Finder (using Tomohiko Sakamoto's algorithm) 
+// Determination of the day of the week (using Tomohiko Sakamoto's algorithm)
 
 #include<iostream>
 #include<sstream>
 using namespace std;
 
-// user inputs
-int iDay = 0;
-int iMonth = 0;
-int iYear = 0;
+// Global Variables
+int gDay = 0;
+int gMonth = 0;
+int gYear = 0;
+string gDayName = "";
 
 string get_input(string);
-bool test_inputValidity(string);
+bool test_inputValidity(string, string);
 string process_dayOfWeek(int,int,int);
 void display_results(int,int,int,string);
+void input_loop(int);
+void update_globalVariables(string,int);
 
 int main()
 {
+    cout << "Determination of the day of the week using Tomohiko Sakamoto's algorithm\n\n";
+
+    input_loop(1);
 
     return 0;
 }
 
+void input_loop(int stage)
+{
+    if(stage == 1)
+    {
+        if(!test_inputValidity("Day", get_input("Day"))) input_loop(1);
+        else input_loop(2);
+    }
+    else if (stage == 2)
+    {
+        if(!test_inputValidity("Month", get_input("Month"))) input_loop(2);
+        else input_loop(3);
+    }
+    else if (stage == 3)
+    {
+        if(!test_inputValidity("Year", get_input("Year"))) input_loop(3);
+        else display_results(gDay, gMonth, gYear, process_dayOfWeek(gDay, gMonth, gYear));
+    }
+}
+
 string get_input(string inputName)
 {
-    cout << "please input\n";
-    cout << inputName << ": ";
+    if(inputName == "Day") cout << "please input\n"; // show this statement on first iteration only.
+    cout << '\t' << inputName << ": ";
 
     string str;
     getline(cin, str);
@@ -32,17 +56,32 @@ string get_input(string inputName)
     return str;
 }
 
-bool test_inputValidity(string input)
+bool test_inputValidity(string inputType, string input)
 {
     bool isValid = false;
 
     int testNum;
     stringstream(input) >> testNum;
 
-    if(testNum != 0 || input == "0") isValid = true;
-    cout << testNum;
+    if((testNum != 0 || input == "0") && (testNum >= 0))
+    {
+        // okay, now we know it is a number, now to check if it is in acceptable ranges.
+        if((inputType == "Day") && ((testNum > 0) && (testNum < 32))) isValid = true;
+        else if((inputType == "Month") && ((testNum > 0) && (testNum < 13))) isValid = true;
+        else if((inputType == "Year") && ((testNum > 0) && (testNum < 2100))) isValid = true;
+        else cout << "Error @ test_inputValidity():\ninputType: " << inputType << "\ntestNum: " << testNum << endl;
+    }
+
+    if(isValid) update_globalVariables(inputType, testNum);
 
     return isValid;
+}
+
+void update_globalVariables(string iType, int input)
+{
+    if(iType == "Day") gDay = input;
+    else if (iType == "Month") gMonth = input;
+    else if (iType == "Year") gYear = input;
 }
 
 string process_dayOfWeek(int day, int month, int year)
@@ -50,15 +89,16 @@ string process_dayOfWeek(int day, int month, int year)
     static int t[] = {0,3,2,5,0,3,5,1,4,6,2,4};
     year -= month < 3;
 
+    string dayNameArray[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+
     int dayNum = (year + (year/4) - (year/100) + (year/400) + t[month-1] + day) % 7;
 
-    const char* dayName[] = {"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
-
-    return dayName[dayNum];
+    return dayNameArray[dayNum];
 }
 
 void display_results(int d, int m, int y, string dName)
 {
-    string monthName[] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
-    cout << "The " << d << " of " << monthName[m] << " " << y << " was a " << dName << endl;
+    string monthNameArray[] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+
+    cout << "\n\tThe " << d << " of " << monthNameArray[m-1] << " " << y << " was a " << dName << endl;
 }
