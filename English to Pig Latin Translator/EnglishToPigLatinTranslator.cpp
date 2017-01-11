bc03ab632ff335d620cefaf0a47e12e4bf800bce
oa <Narroe@users.noreply.github.com>
2017-01-11 19:25:11 +0000

    Update EnglishToPigLatinTranslator.cpp
    still have a small error where the last word is missing it's last character

--- English to Pig Latin Translator/EnglishToPigLatinTranslator.cpp
+++ English to Pig Latin Translator/EnglishToPigLatinTranslator.cpp
@@ -74,27 +74,64 @@ void separate_string(string str)
 
 string translate_word(string input, bool isLast)
 {
+    cout << "iL:" << isLast << "|" << input << "|"; // test
+
     string output = "";
     unsigned int index = 0;
+    unsigned int strLength = 0;
 
-    // is the first letter a consonant
-    if(!isLetterVowel(input[0]))
-    {
-        // is the second letter a consonant
-        if(!isLetterVowel(input[1])) index = 2;
-        else index = 1;
+    // work out string length
+    if(isLast) strLength = input.length();
+    else strLength = input.length()-1;
 
-    }
-    // if first letter is a vowel, add "-yay" to the end of the word
-    else
-    {
-        index = 0;
-    }
+    /* work out what character types the word starts with
+        i.e. are there 1, 2, or 3 consonants in a row
+        at the start of the word, or does it begin with
+        a vowel.
+        e.g. 1-"hello", 2-"there", 3-"school", 0-"apple" */
+
+    // 1st first letter is a vowel
+    if(isLetterVowel(input[0])) index = 0;
+    // 1st letter is a consonant (& second letter is a vowel)
+    if(!isLetterVowel(input[0]) && isLetterVowel(input[1])) index = 1;
+    // 1st && 2nd letters are consonants (& 3rd letter is a vowel)
+    if (!isLetterVowel(input[0]) && !isLetterVowel(input[1]) && isLetterVowel(input[2])) index = 2;
+    // 1st, 2nd, && 3rd letters are consonants
+    if (!isLetterVowel(input[0]) && !isLetterVowel(input[1]) && !isLetterVowel(input[2]) && isLetterVowel(input[3])) index = 3;
 
     // string population loop
-    for(unsigned int i = index; i<input.length(); i++)
+    for(unsigned int i = 0; i<strLength; i++)
     {
-        output += input[i+index];
+        // as long as (i+index) less than input.length();
+        if((i+index)<strLength) output += input[i+index];
+        else
+        {
+            // suffix appropriate end
+            if(index == 0) output += "-yay";
+            else if(index == 1)
+            {
+                output += "-";
+                output += input[0];
+                output += "ay";
+            }
+            else if(index == 2)
+            {
+                output += "-";
+                output += input[0];
+                output += input[1];
+                output += "ay";
+            }
+            else if(index == 3)
+            {
+                output += "-";
+                output += input[0];
+                output += input[1];
+                output += input[2];
+                output += "ay";
+            }
+
+            break; // break loop.
+        }
     }
 
     return output;
@@ -104,11 +141,13 @@ bool isLetterVowel(char l)
 {
     bool isVowel = false;
     if(l == 'a' || l == 'e' || l == 'i' || l == 'o' || l == 'u') isVowel = true;
+    //cout << "[" << isVowel << "]\n";
     return isVowel;
 }
 
 void store_translatedString(string input)
 {
+    //cout << ": " << input;
     tStr += (input + " ");
 }
 
