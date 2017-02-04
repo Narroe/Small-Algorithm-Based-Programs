165e00f707800675c429eab56de80c7effc03846
oa <Narroe@users.noreply.github.com>
2017-02-04 06:00:01 +0000

    Update PalindromeDetector.cpp

--- Palindrome Detector/PalindromeDetector.cpp
+++ Palindrome Detector/PalindromeDetector.cpp
@@ -4,15 +4,10 @@
 
 using namespace std;
 
+string gWord = "";
 void display_welcomeMsg(void);
 string get_input();
 
-
-int main()
-{
-    display_welcomeMsg();
-    return 0;
-}
 
 void display_welcomeMsg()
 {
@@ -28,3 +23,24 @@ string get_input()
     return input;
 }
 
+bool test_string(string input)
+{
+    gWord = input;
+
+    bool isPalindrome = false;
+
+    return isPalindrome;
+}
+
+void display_results(bool isPal)
+{
+    if(isPal) cout << "\n\n\t\"" << gWord << "\" IS a palindrome.\n\n";
+    else cout << "\n\n\t\"" << gWord << "\" is NOT a palindrome.\n\n";
+}
+
+int main()
+{
+    display_welcomeMsg();
+    display_results(test_string(get_input()));
+    return 0;
+}
